716f285ecbbb22ffc6194d653dfba81aa4251f8c
Dohvakin <yashkaveeshwar@gmail.com>
2022-01-06 12:05:41 +0530

    commit all the modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./admin/component/Nav";
+import Menu from "./admin/component/Menu";
+import {BrowserRouter, Route} from "react-router-dom";
+import Products from "./admin/Products";
+import Main from "./main/Main";
+import ProductCreate from "./admin/ProductCreate";
+import ProductEdit from "./admin/ProductEdit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <BrowserRouter>
+                <Route path='/' exact component={Main}/>
+                <Route path='/admin/products' exact component={Products}/>
+                <Route path='/admin/products/create' exact component={ProductCreate}/>
+                <Route path='/admin/products/:id/edit' exact component={ProductEdit}/>
+            </BrowserRouter>
     </div>
   );
 }
--- src/admin/ProductEdit.tsx
+++ src/admin/ProductEdit.tsx
@@ -1,16 +1,31 @@
-import React, {SyntheticEvent, useState} from 'react';
+import React, {PropsWithRef, SyntheticEvent, useEffect, useState} from 'react';
 import Wrapper from "./Wrapper";
-import {Redirect} from "react-router-dom";
+import {Redirect} from 'react-router-dom';
+import {Product} from "../interfaces/product";
 
-const ProductCreate = () => {
+const ProductsEdit = (props: PropsWithRef<any>) => {
     const [title, setTitle] = useState('');
     const [image, setImage] = useState('');
     const [redirect, setRedirect] = useState(false);
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch(`http://localhost:8000/api/products/${props.match.params.id}`);
+
+                const product: Product = await response.json();
+
+                setTitle(product.title);
+                setImage(product.image)
+            }
+        )();
+    }, []);
+
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
-        await fetch('http://localhost:8000/api/products/', {
-            method: 'POST',
+        await fetch(`http://localhost:8000/api/products/${props.match.params.id}`, {
+            method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 title,
@@ -24,18 +39,21 @@ const ProductCreate = () => {
     if (redirect) {
         return <Redirect to={'/admin/products'}/>
     }
+
     return (
         <Wrapper>
             <form onSubmit={submit}>
                 <div className="form-group">
                     <label>Title</label>
                     <input type="text" className="form-control" name="title"
+                           defaultValue={title}
                            onChange={e => setTitle(e.target.value)}
                     />
                 </div>
                 <div className="form-group">
                     <label>Image</label>
                     <input type="text" className="form-control" name="image"
+                           defaultValue={image}
                            onChange={e => setImage(e.target.value)}
                     />
                 </div>
@@ -45,4 +63,4 @@ const ProductCreate = () => {
     );
 };
 
-export default ProductCreate;
+export default ProductsEdit;
